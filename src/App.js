f673830e587b43534aaa89fbc707be228935713b
Nepcam <cameron.nepe@gmail.com>
2019-10-03 14:19:54 +1300

    adding files for github pages

--- src/App.js
+++ src/App.js
@@ -34,6 +34,7 @@ class App extends Component {
       
     return (
       <div className='App'>
+        <h1>Monsters Rolodex</h1>
         <SearchBox
         placeholder='search monster'
         handleChange={this.handleChange}
