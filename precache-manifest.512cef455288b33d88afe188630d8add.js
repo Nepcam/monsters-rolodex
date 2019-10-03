e709d94640e65019b5fa2c9f6661c9c0586e8bc4
Nepcam <cameron.nepe@gmail.com>
2019-10-03 14:18:52 +1300

    Updates

---
+++ precache-manifest.512cef455288b33d88afe188630d8add.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "324b15c1315456288e0b80363211a75a",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "fc384e04d38e09acaaea",
+    "url": "/monsters-rolodex/static/css/main.6a1e7869.chunk.css"
+  },
+  {
+    "revision": "8ae996a654f27ebbabb8",
+    "url": "/monsters-rolodex/static/js/2.2c8a6238.chunk.js"
+  },
+  {
+    "revision": "fc384e04d38e09acaaea",
+    "url": "/monsters-rolodex/static/js/main.9eab3cb3.chunk.js"
+  },
+  {
+    "revision": "c4a009f5d3a5fc9c4346",
+    "url": "/monsters-rolodex/static/js/runtime-main.205b164d.js"
+  }
+]);
